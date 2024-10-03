ef61bbfebad8f5a3bb51c87b05bea41d8a223f43
Aleksandar <pa200059d@student.etf.bg.ac.rs>
2024-10-03 22:43:38 +0200

    E1000 ctrl reagister

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -1,3 +1,5 @@
+#include <assert.h>
+
 #include "types.h"
 #include "riscv.h"
 #include "memlayout.h"
@@ -10,18 +12,23 @@
 
 struct spinlock e1000_lock;
 
-static volatile uint32* e1000_regs = 0;
+static volatile union e1000_reg* regs= 0;
 
 void
 e1000_init(volatile union pcie_config_hdr* hdr) {
+	printf("Configuring e1000 network card\n");
+	assert(sizeof(union e1000_reg) == sizeof(uint32));	
+
 	initlock(&e1000_lock, "e1000_spinlock");
-	hdr->t0.cmd = E1000_PCIE_CMD_IO_ENABLE |
-	              E1000_PCIE_CMD_MMIO_ENABLE | 
-	              E1000_PCIE_CMD_BUS_MASTERING_ENABLE;
+	hdr->t0.cmd |= E1000_PCIE_CMD_IO_ENABLE |
+	               E1000_PCIE_CMD_MMIO_ENABLE | 
+	               E1000_PCIE_CMD_BUS_MASTERING_ENABLE;
 	__sync_synchronize();
 	hdr->t0.base_addr_regs[0] = E1000_BASE | E1000_PCIE_BAR0_64BIT;
 	hdr->t0.base_addr_regs[1] = 0;
 	__sync_synchronize();
 
-	e1000_regs = (uint32*) E1000_BASE;
+	regs = (union e1000_reg*) E1000_BASE;
+
+
 }
--- kernel/e1000.h
+++ kernel/e1000.h
@@ -24,5 +24,54 @@
 #define E1000_MTA      (0x05200/4)  /* Multicast Table Array - RW Array */
 #define E1000_RA       (0x05400/4)  /* Receive Address - RW Array */
 
+// E1000 Control Register
+union __attribute__((packed)) e1000_reg {
+	struct __attribute__((packed)) {
+		uint32 fd : 1;
+		uint32 reserved0 : 2;
+		uint32 lrst : 1;
+		uint32 reserved1 : 1;
+		uint32 asde : 1;
+		uint32 slu : 1;
+		uint32 ilos : 1;
+		uint32 speed : 2;
+		uint32 reserved2 : 1;
+		uint32 frcspd : 1;
+		uint32 frcdplx : 1;
+		uint32 reserved3 : 5;
+		uint32 sdp0_data : 1;
+		uint32 sdp1_data : 1;
+		uint32 advd3wuc : 1;
+		uint32 en_phy_pwr_mgmt : 1;
+		uint32 sdp0_iodir : 1;
+		uint32 sdp1_iodir : 1;
+		uint32 reserved4 : 2;
+		uint32 rst : 1;
+		uint32 rfce : 1;
+		uint32 tfce : 1;
+		uint32 reserved5 : 1;
+		uint32 vme : 1;
+		uint32 phy_rst : 1;
+	} ctrl;
+};
+struct __attribute__((packed)) tx_desc {
+	uint64    addr;
+	uint16  length;
+	uint8      cso;
+	uint8      cmd;
+	uint8   status;
+	uint8 reserved;
+	uint8      css;
+	uint16 special;
+};
+
+struct __attribute__((packed)) rx_desc {
+	uint64     addr;
+	uint16   length;
+	uint16 checksum;
+	uint8    status;
+	uint8    errors;
+	uint16  special;
+};
 
 extern struct spinlock e1000_lock;
