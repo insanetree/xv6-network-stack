5b75a18f0ab3cf8cd6f704fa3cdadd67a88568e1
Aleksandar <pa200059d@student.etf.bg.ac.rs>
2024-10-12 17:03:40 +0200

    TX init

--- kernel/e1000.c
+++ kernel/e1000.c
@@ -12,12 +12,22 @@
 
 struct spinlock e1000_lock;
 
-static volatile union e1000_reg* regs= 0;
+static volatile uint32* regs = 0;
+static volatile struct e1000_ctrl* ctrl = 0;
+
+static volatile struct e1000_tctl* tctl = 0;
+static volatile uint32* tdbal = 0;
+static volatile uint32* tdbah = 0;
+static volatile uint32* tdlen = 0;
+static volatile uint32* tdh   = 0;
+static volatile uint32* tdt   = 0;
+
+#define TX_RING_SIZE 32
+struct tx_desc tx_ring[TX_RING_SIZE] __attribute__((aligned(16)));
 
 void
 e1000_init(volatile union pcie_config_hdr* hdr) {
 	printf("Configuring e1000 network card\n");
-	assert(sizeof(union e1000_reg) == sizeof(uint32));	
 
 	initlock(&e1000_lock, "e1000_spinlock");
 	hdr->t0.cmd |= E1000_PCIE_CMD_IO_ENABLE |
@@ -28,7 +38,42 @@ e1000_init(volatile union pcie_config_hdr* hdr) {
 	hdr->t0.base_addr_regs[1] = 0;
 	__sync_synchronize();
 
-	regs = (union e1000_reg*) E1000_BASE;
+	regs = (uint32*) E1000_BASE;
+	ctrl = (struct e1000_ctrl*) &regs[E1000_CTRL];
 
+	ctrl->rst = 0;
+	__sync_synchronize();
+	ctrl->asde = 1;
+	ctrl->slu = 1;
+	ctrl->ilos = 0;
+	ctrl->vme = 0;
+	__sync_synchronize();
 
+	// TX init
+	assert(sizeof(struct tx_desc) == 16);
+	assert(sizeof(tx_ring) % 128 == 0);
+
+	tctl  = (struct e1000_tctl*)&regs[E1000_TCTL];
+	tdbal = &regs[E1000_TDBAL];
+	tdbah = &regs[E1000_TDBAH];
+	tdlen = &regs[E1000_TDLEN];
+	tdh   = &regs[E1000_TDH];
+	tdt   = &regs[E1000_TDT];
+
+	memset(&tx_ring, 0, TX_RING_SIZE * sizeof(struct tx_desc));
+	for(uint32 i = 0 ; i < TX_RING_SIZE ; i++) {
+		tx_ring[i].status |= TX_DESC_STATUS_DD;
+	}
+
+	*tdbal = (uint32)((uint64)&tx_ring & 0xffffffff);
+	*tdbah = (uint32)((uint64)&tx_ring >> 32);
+	*tdlen = sizeof(tx_ring);
+	*tdh   = 0;
+	*tdt   = 0;
+
+	// TX control
+	tctl->en = 1;
+	tctl->psp = 1;
+
+	return;
 }
--- kernel/e1000.h
+++ kernel/e1000.h
@@ -18,6 +18,7 @@
 #define E1000_RDLEN    (0x02808/4)  /* RX Descriptor Length - RW */
 #define E1000_RSRPD    (0x02C00/4)  /* RX Small Packet Detect Interrupt */
 #define E1000_TDBAL    (0x03800/4)  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    (0x03804/4)  /* TX Descriptor Base Address High - RW */
 #define E1000_TDLEN    (0x03808/4)  /* TX Descriptor Length - RW */
 #define E1000_TDH      (0x03810/4)  /* TX Descriptor Head - RW */
 #define E1000_TDT      (0x03818/4)  /* TX Descripotr Tail - RW */
@@ -25,44 +26,70 @@
 #define E1000_RA       (0x05400/4)  /* Receive Address - RW Array */
 
 // E1000 Control Register
-union __attribute__((packed)) e1000_reg {
-	struct __attribute__((packed)) {
-		uint32 fd : 1;
-		uint32 reserved0 : 2;
-		uint32 lrst : 1;
-		uint32 reserved1 : 1;
-		uint32 asde : 1;
-		uint32 slu : 1;
-		uint32 ilos : 1;
-		uint32 speed : 2;
-		uint32 reserved2 : 1;
-		uint32 frcspd : 1;
-		uint32 frcdplx : 1;
-		uint32 reserved3 : 5;
-		uint32 sdp0_data : 1;
-		uint32 sdp1_data : 1;
-		uint32 advd3wuc : 1;
-		uint32 en_phy_pwr_mgmt : 1;
-		uint32 sdp0_iodir : 1;
-		uint32 sdp1_iodir : 1;
-		uint32 reserved4 : 2;
-		uint32 rst : 1;
-		uint32 rfce : 1;
-		uint32 tfce : 1;
-		uint32 reserved5 : 1;
-		uint32 vme : 1;
-		uint32 phy_rst : 1;
-	} ctrl;
+struct __attribute__((packed)) e1000_ctrl {
+	uint32 fd : 1;
+	uint32 reserved0 : 2;
+	uint32 lrst : 1;
+	uint32 reserved1 : 1;
+	uint32 asde : 1;
+	uint32 slu : 1;
+	uint32 ilos : 1;
+	uint32 speed : 2;
+	uint32 reserved2 : 1;
+	uint32 frcspd : 1;
+	uint32 frcdplx : 1;
+	uint32 reserved3 : 5;
+	uint32 sdp0_data : 1;
+	uint32 sdp1_data : 1;
+	uint32 advd3wuc : 1;
+	uint32 en_phy_pwr_mgmt : 1;
+	uint32 sdp0_iodir : 1;
+	uint32 sdp1_iodir : 1;
+	uint32 reserved4 : 2;
+	uint32 rst : 1;
+	uint32 rfce : 1;
+	uint32 tfce : 1;
+	uint32 reserved5 : 1;
+	uint32 vme : 1;
+	uint32 phy_rst : 1;
 };
+
+// E1000 Transmit Control Register
+struct __attribute__((packed)) e1000_tctl {
+	uint32 reserved0 : 1;
+	uint32 en : 1;
+	uint32 reserved1 : 1;
+	uint32 psp : 1;
+	uint32 ct : 8;
+	uint32 cold : 10;
+	uint32 swxoff : 1;
+	uint32 reserved2 : 1;
+	uint32 rtcl : 1;
+	uint32 nrtu : 1;
+	uint32 reserved3 : 6;
+};
+
+#define TX_DESC_CMD_EOP  0x01 /* End Of Packet */
+#define TX_DESC_CMD_IFCS 0x02 /* Insert FCS*/
+#define TX_DESC_CMD_IC   0x04 /* Insert Checksum */
+#define TX_DESC_CMD_RS   0x08 /* Report Status */
+#define TX_DESC_CMD_RPS  0x10 /* Report Packet Sent */
+#define TX_DESC_CMD_DEXT 0x20 /* Extension (set to 0) */
+#define TX_DESC_CMD_VLE  0x40 /* VLAN Packet Enable */
+#define TX_DESC_CMD_IDE  0x80 /* Interrupt Delay Enable */
+
+#define TX_DESC_STATUS_DD 0x1 /* Descriptor Done */
+#define TX_DESC_STATUS_EX 0x2 /* Excess Collision */
+#define TX_DESC_STATUS_LC 0x4 /* Late Collision */
 struct __attribute__((packed)) tx_desc {
-	uint64    addr;
-	uint16  length;
-	uint8      cso;
-	uint8      cmd;
-	uint8   status;
-	uint8 reserved;
-	uint8      css;
-	uint16 special;
+	uint64    addr;      // Address
+	uint16  length;      // Length
+	uint8      cso;      // Checksum offset
+	uint8      cmd;      // Command
+	uint8   status : 4;  // Status
+	uint8 reserved0 : 4; // Reserved, write to 0
+	uint8      css;      // Checksum Start
+	uint16 special;      // Special
 };
 
 struct __attribute__((packed)) rx_desc {
